61a21377fb0f3a4264d9b89a638ec8ea41f584e8
EC2 Default User <ec2-user@ip-172-31-40-127.ec2.internal>
2022-03-26 02:49:46 +0000

    [Finish]課題３

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "86762b643a1a42b2bbfef6d5e19c471d.vfs.cloud9.us-east-1.amazonaws.com"
+
+  config.active_job.queue_adapter = :inline
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users
+  root to: 'homes#top'
+  get "/home/about" => "homes#about"
+
+  resources :users, only: [:show, :edit, :index, :update]
+  resources :books, only: [:new, :index, :show, :create, :edit, :update, :destroy]
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
